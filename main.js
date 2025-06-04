d8371be0ded2148bf29572898c74c36b19f40efa
L4P <209110931+LoveForPeeps@users.noreply.github.com>
2025-06-04 21:38:20 +0530

    Added round logic

--- main.js
+++ main.js
@@ -16,3 +16,28 @@ function getComputerChoice() {
 function getUserChoice() {
     return prompt("Rock, paper, scissors?");
 }
+
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log(`Its a tie! You both picked ${humanChoice}.`);
+        playRound(getUserChoice(), getComputerChoice());
+        return;
+    }
+
+    if (humanChoice === "rock" && computerChoice != "paper") {
+        console.log(`You won! Rock beat ${computerChoice}.`)
+    } else if (humanChoice === "paper" && computerChoice != "scissors") {
+        console.log(`You won! Paper beat ${computerChoice}.`)
+    } else if (humanChoice === "scissors" && computerChoice != "rock") {
+        console.log(`You won! Scissors beat ${computerChoice}.`)
+    } else {
+        console.log(`You lost! ${humanChoice} loses to ${computerChoice}.`);
+        computerScore++;
+        return;
+    }
+
+    userScore++;
+}
